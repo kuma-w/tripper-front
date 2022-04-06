f568b49bd39b09445e5f471f215ee6e0f5666273
dev-woong <dev-woong@naver.com>
2022-04-06 17:28:20 +0900

    New : 기본 폴더 트리

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Map from "./Map"
+import Sidebar from "./Sidebar"
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Map />
+    </div>
+  )
+}
+
+export default App
