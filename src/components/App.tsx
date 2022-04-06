2aa102612aad42d970324317eda9f851fe96424a
dev-woong <dev-woong@naver.com>
2022-04-06 17:55:33 +0900

    New : MUI 임시 적용

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,10 @@
 import React from "react"
-import Map from "./Map"
-import Sidebar from "./Sidebar"
+import PersistentDrawer from "./PersistentDrawer"
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
-      <Map />
+      <PersistentDrawer />
     </div>
   )
 }
